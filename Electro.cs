afae3e1a1d7fe00c08c9ef3dea2cb62669dbbf04
nova <147922358+tartanova@users.noreply.github.com>
2023-11-05 07:51:06 +0100

    electro gun for starship

---
+++ Electro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Electro : MonoBehaviour
+{
+    private float Damage;
+    //звук
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip ShotClip;
+    private void Start()
+    {
+        Damage = PlayerPrefs.GetFloat("PDamage") /4 * PlayerPrefs.GetFloat("PDamBo");
+        StartCoroutine(Sound());
+    }
+    IEnumerator Sound()
+    {
+        while (true)
+        {
+            audioSource.PlayOneShot(ShotClip);
+            yield return new WaitForSeconds(1);
+        }
+    }
+    private void OnParticleCollision(GameObject coll)
+    {
+        if (coll.gameObject.tag == "Meteorit") { coll.transform.GetComponent<EnemyScript>().TakeDamage(Damage);}
+    }
+   
+}
